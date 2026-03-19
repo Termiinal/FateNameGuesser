81f414a7a448931ca2f7a5ae1a10ad849648848b
Termiinal <lgnvelocity@aol.com>
2026-03-18 21:55:14 -0400

    Update puzzles.js

--- puzzles.js
+++ puzzles.js
@@ -1,11 +1,11 @@
-
 const puzzles = [
-{
-image:"images/puzzle1.png",
-answers:[],
-hints:[
-{type:"text",title:"Region",content:""},
-{type:"text",title:"Class",content:""}
-]
-}
+  {
+    image: "images/puzzle1.png", // Ensure 'images' is lowercase if your folder is lowercase
+    answers: ["test"],
+    hints: [
+      { type: "text", title: "Region", content: "Test" },
+      { type: "text", title: "Class", content: "Test" }
+      }
+    ]
+  }
 ];
