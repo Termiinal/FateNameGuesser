e01ed0c03aee5f6b74344252c628882acdbfbf5a
Termiinal <lgnvelocity@aol.com>
2026-03-18 21:34:13 -0400

    Update puzzles.js

--- FateNameGuesser/puzzles.js
+++ FateNameGuesser/puzzles.js
@@ -2,14 +2,10 @@
 const puzzles = [
 {
 image:"images/puzzle1.png",
-answers:["quetzalcoatl","quetz"],
+answers:[],
 hints:[
-{type:"text",title:"Region",content:"Central America"},
-{type:"text",title:"Class",content:"Rider"},
-{type:"video",title:"Noble Phantasm Video",content:"https://youtu.be/A-DJpNYg7bY"},
-{type:"text",title:"Skill Name",content:"Charisma of the Sun EX"},
-{type:"text",title:"Biography Blurb",content:"___ led her people to prosperity as a good god, but eventually she was defeated by the god Tezcatlipoca and vanished from the Aztec civilization, leaving behind a prophecy that she would return."},
-{type:"text",title:"Answer",content:"quetzalcoatl"}
+{type:"text",title:"Region",content:""},
+{type:"text",title:"Class",content:""}
 ]
 }
 ];
